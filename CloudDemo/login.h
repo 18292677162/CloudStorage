351365eff933b15a2385b00eff0410bd5f88d5da
18292677162 <2254095453@qq.com>
2019-07-30 22:40:38 +0800

    -

---
+++ CloudDemo/login.h
@@ -0,0 +1,26 @@
+#ifndef LOGIN_H
+#define LOGIN_H
+
+#include <QDialog>
+
+namespace Ui {
+class Login;
+}
+
+class Login : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Login(QWidget *parent = nullptr);
+    ~Login();
+
+protected:
+    // 绘图事件函数
+    void paintEvent(QPaintEvent *event);
+
+private:
+    Ui::Login *ui;
+};
+
+#endif // LOGIN_H
