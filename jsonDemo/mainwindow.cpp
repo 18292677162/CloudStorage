c9fa5adb95135871b9431abd3dd36d1122a84880
18292677162 <2254095453@qq.com>
2019-08-03 00:03:22 +0800

    -

--- jsonDemo/mainwindow.cpp
+++ jsonDemo/mainwindow.cpp
@@ -8,6 +8,7 @@
 #include <QNetworkAccessManager>
 #include <QNetworkReply>
 #include <QNetworkRequest>
+#include <QCryptographicHash>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -58,7 +59,7 @@ MainWindow::MainWindow(QWidget *parent) :
         }
     }
 
-    // 创建 manager 对象，做 post, get
+    // 创建 manager 对象，做 post, get，整个项目只需一个
     QNetworkAccessManager *manager = new QNetworkAccessManager(this);
     // 初始化一个 QNetworkRequest 类
     QNetworkRequest res;
@@ -67,15 +68,42 @@ MainWindow::MainWindow(QWidget *parent) :
     res.setHeader(QNetworkRequest::UserAgentHeader,
                   "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36");
     res.setUrl(QUrl("http://www.baidu.com:80"));
+    // 接收服务器数据
     QNetworkReply *reply = manager->post(res, "");
 
     // 读服务器回复数据
     connect(reply, &QNetworkReply::readyRead, this, [=]()
     {
-       // 读取数据
+        // 读取数据
+        QVariant str = reply->header(QNetworkRequest::LocationHeader);
+        // 数据 - 不包括 Header
         QByteArray data2 = reply->readAll();
         qDebug() << data2;
+        qDebug() << str.toString();
     });
+
+    // MD5
+    QByteArray data3 = "hello,world";
+    QCryptographicHash hash(QCryptographicHash::Md5);
+    hash.addData("你好");
+
+    // hash 计算
+    QByteArray arry = hash.result();
+    qDebug() << arry;
+    arry = arry.toHex();
+    qDebug() << arry;
+
+    // MD5
+    QByteArray arry1 = QCryptographicHash::hash("hello, world", QCryptographicHash::Md5);
+    qDebug() << arry1.toHex();
+
+    // Base64
+    QByteArray base = "hello,world";
+    base = base.toBase64();
+    qDebug() << base.data();
+    // 解码
+    base = QByteArray::fromBase64(base);
+    qDebug() << base.data();
 }
 
 MainWindow::~MainWindow()
